f2112880f688cdb8a77fa682c5681c0caea0ac43
Douglas Berkley <douglasmberkley@gmail.com>
2022-02-27 21:51:42 +0900

    added user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+puts 'Creating users....'
+User.create!(
+  email: 'douglas.berkley@lewagon.org'
+  password: '123123',
+  nickname: 'dmbf29'
+)
+
+User.create!(
+  email: 'yann.klein@lewagon.org'
+  password: '123123',
+  nickname: 'yannklein'
+)
+
+User.create!(
+  email: 'trouni@lewagon.org'
+  password: '123123',
+  nickname: 'trouni'
+)
+puts "...created #{User.count} users."
